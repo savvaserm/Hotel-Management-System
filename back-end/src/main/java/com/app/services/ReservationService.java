07dd975a90132d350acdc4a49cfd62b7bdb47a57
Savvas Ermidis <39909089+savvaserm@users.noreply.github.com>
2019-07-30 17:42:05 +0300

    Add files via upload

--- back-end/src/main/java/com/app/services/ReservationService.java
+++ back-end/src/main/java/com/app/services/ReservationService.java
@@ -84,7 +84,7 @@ public class ReservationService {
         List<Reservation> reservations = reservationRepository.findByCustomer(customer);
         int count = reservations.size() + 1;
         System.out.println("\nReservations made by " + customer.getLastname() + ": " + count);
-        if ( count >= 3) {
+        if (count >= 3) {
             customer.setType("REGULAR");
         } else {
             customer.setType("NEW");
@@ -95,15 +95,15 @@ public class ReservationService {
 
 
         //AN TO KLEINEI 90 MERES NWRITERA EXEI 0% EKPTWSH,GIA 90 MEXRI 140 10%,GIA >140 20%
-        if( difference < 90) {
+        if (difference < 90) {
             discount2 = 0.0;
-        } else if ( difference <= 140 ) {
+        } else if (difference <= 140) {
             discount2 = 0.1;
         } else {
             discount2 = 0.2;
         }
 
-        if(room.getCancel()) {
+        if (room.getCancel()) {
             discount3 = 0.0;
             System.out.println("If reservation is cancelled, money will be refunded");
 
@@ -113,19 +113,18 @@ public class ReservationService {
         }
 
         quantity = room.getRoom_roomtype().getQuantity().getAmount();
-        if ( quantity < 1) {
+        if (quantity < 1) {
             extraCost = 40.0;
-        } else if ( quantity < 3 ) {
+        } else if (quantity < 3) {
             extraCost = 20.0;
         } else {
             extraCost = 0.0;
         }
 
 
-
         //DEN VRISKEI TO ROOM ID, CHECKAREI MONO TIS HMEROMHNIES KAI STELNEI PISW OLA TA RESERVATIONS GIAYTES TIS HMEROMHNIES
         List<Reservation> checkRes = reservationRepository.findByRoomAndDate(room, checkin, checkout);
-        if( checkRes.size() >= 1 ) {
+        if (checkRes.size() >= 1) {
             System.out.println("Reservations for given dates: " + checkRes.size());
         } else {
             room.setAvailability(true);
@@ -134,7 +133,7 @@ public class ReservationService {
 
         if (!room.getAvailability()) {
 
-            message = "Room is not available at given dates!" ;
+            message = "Room is not available at given dates!";
             System.out.println(message);
 
             //15% DISCOUNT STOUS REGULAR PELATES
@@ -149,8 +148,8 @@ public class ReservationService {
 
             message = "Room booked!";
             quantity = room.getRoom_roomtype().getQuantity().getAmount();
-            price = (double)reservation.getNights()*room.getRoom_roomtype().getPrice() + room.getRoom_roomtype().getPrice();
-            discount = 15*price/100 + discount2*price + discount3*price;
+            price = (double) reservation.getNights() * room.getRoom_roomtype().getPrice() + room.getRoom_roomtype().getPrice();
+            discount = 15 * price / 100 + discount2 * price + discount3 * price;
             total = price - discount + extraCost;
             reservation.setTotal(total);
 
@@ -175,8 +174,8 @@ public class ReservationService {
             room.getRoom_roomtype().getQuantity().setAmount(quantity - 1);
 
 
-            price = (double)reservation.getNights()*room.getRoom_roomtype().getPrice() + room.getRoom_roomtype().getPrice();
-            total = price - discount2*price - discount3*price + extraCost;
+            price = (double) reservation.getNights() * room.getRoom_roomtype().getPrice() + room.getRoom_roomtype().getPrice();
+            total = price - discount2 * price - discount3 * price + extraCost;
             reservation.setTotal(total);
             message = "Room booked!";
 
@@ -199,12 +198,12 @@ public class ReservationService {
         Optional<Reservation> optRes = reservationRepository.findById(reservation.getRoom_reservationId());
         Reservation res = optRes.get();
 
-        if(res.getRoom().getCancel()) {
+        if (res.getRoom().getCancel()) {
 
-            System.out.println("\nReservation with id: [" +  res.getRoom_reservationId() + "]    cancelled (MONEY WILL BE REFUNDED)!");
             reservationRepository.delete(res);
             res.getRoom().setAvailability(true);
             res.getRoom().getRoom_roomtype().getQuantity().setAmount(quantity + 1);
+            System.out.println("\nReservation with id: [" + res.getRoom_reservationId() + "]    cancelled (MONEY WILL BE REFUNDED)!");
 
         } else {
 
